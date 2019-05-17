e35bebe7292dcce5343cfe4908d70f8394f4dc34
Phillip Whelan <phillip.whelan@timestamp.cl>
2019-05-17 16:06:37 -0400

    cleanup.

--- midi.go
+++ midi.go
@@ -5,11 +5,11 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
-const MidiTypeName = "midi"
+const midiTypeName = "midi"
 
 // Registers MIDI Type
 func registerMIDI(L *lua.LState) {
-	miditype := L.NewTypeMetatable(MidiTypeName)
+	miditype := L.NewTypeMetatable(midiTypeName)
 	// methods
 	L.SetField(miditype,
 		"__index",
@@ -18,7 +18,6 @@ func registerMIDI(L *lua.LState) {
 				ud := L.CheckUserData(1)
 				if m, ok := ud.Value.(*midi.MIDI); !ok {
 					L.ArgError(1, "MIDI expected")
-					return 0
 				} else {
 					if L.GetTop() == 5 {
 						channel := L.CheckInt64(2)
@@ -26,7 +25,6 @@ func registerMIDI(L *lua.LState) {
 						hb := L.CheckInt64(4)
 						lb := L.CheckInt64(5)
 						m.Send(channel, status, hb, lb)
-						return 0
 					} else {
 						L.ArgError(1, "4 arguments expected")
 					}
@@ -53,7 +51,7 @@ func userdataMIDI(L *lua.LState, m *midi.MIDI) lua.LValue {
 	t.RawSet(lua.LString("in"), lua.LNil)
 	mud := L.NewUserData()
 	mud.Value = m
-	L.SetMetatable(mud, L.GetTypeMetatable(MidiTypeName))
+	L.SetMetatable(mud, L.GetTypeMetatable(midiTypeName))
 	t.RawSet(lua.LString("out"), mud)
 	t.RawSet(lua.LString("id"), lua.LNumber(m.ID))
 	t.RawSet(lua.LString("name"), lua.LString(m.Info.Name))
