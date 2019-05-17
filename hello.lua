9004fdb1e13ad4edd122ea8a799cdcef59a353de
Phillip Whelan <phillip.whelan@timestamp.cl>
2019-05-17 16:07:47 -0400

    Add joystick support.

--- hello.lua
+++ hello.lua
@@ -1,31 +1,28 @@
 function init ()
-	print("hello")
-	toggel = false
+	toggle = true
 end
 
 vjoy = vjoys[1]
 
 function tick(cur)
-	-- print("tick: ", tostring(cur))
 	if (cur % 100) == 0 then
 		if toggle == true then
 			toggle = false
-			print("WAX ON")
+			print("toggled off")
 		else
 			toggle = true
-			print("WAX OFF")
+			print("toggled on")
 		end
-		-- print("ON")
-		-- midi[1]["out"]:Send(1, 144, 50, 100)
-	elseif (cur % 50) == 0 then
-		-- print("OFF")
-		-- midi[1]["out"]:Send(1, 128, 50, 0)
 	end
 end
 
 function midirecv(dev, msg)
-	print("MIDI RECV'd: ", dev["name"])
-	-- midi[2]["out"]:Send(14, 144, 36, 69)	
+	print("MIDI received: ", dev["id"], " -> ", dev["name"])
+	print(msg.channel, 
+		msg.status, 
+		msg.data[1], 
+		msg.data[2])
+
 	if msg.data[1] == 48 then
 		print("48!")
 		if msg.status == 144 then
@@ -42,6 +39,10 @@ function midirecv(dev, msg)
 end
 
 function joystickrecv(ev)
-	print("GAMEPAD=")
-	print(ev['type'])
+	if ev['type'] == joystick.EVENT_BUTTON then
+		print("BUTTON: " + ev['button'] + " = " + ev['value'])
+	end
+	if ev['type'] == joystick.EVENT_AXIS then
+		print("AXIS: " + ev['axis'] + " = " + ev['value'])
+	end
 end
